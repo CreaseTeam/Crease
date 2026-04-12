a845cae74cbdaf10a1b55958fee61b0793eb449d
Max Raffel <44725991+maxraffel@users.noreply.github.com>
2026-04-12 16:57:34 -0700

    variable shadow offset

--- Assets/Flying/Player/ShadowPosition.cs
+++ Assets/Flying/Player/ShadowPosition.cs
@@ -3,16 +3,17 @@ using UnityEngine;
 public class ShadowPosition : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    [SerializeField] private float shadowDistance = 1f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - 1, player.transform.position.z);
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - shadowDistance, player.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-         this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - 1, player.transform.position.z);
+         this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - shadowDistance, player.transform.position.z);
     }
 }
