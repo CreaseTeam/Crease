9175563afa2ada695c3515a5f980d6a1ef167a1d
Farrah McQueen <128991498+FMcQueen@users.noreply.github.com>
2026-07-28 00:51:22 -0400

    Implement various ambient sfx

---
+++ Assets/Scenes/Blockouts/Farrah's WwiseTesting/Wwise Scripts/SetMusicState.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SetMusicState : MonoBehaviour
+{
+    // Keeps track of region nesting
+    public static List<AK.Wwise.State> RegionStates = new List<AK.Wwise.State>();
+    public AK.Wwise.State OnTriggerEnterState; // this (gameObject's) region
+    public AK.Wwise.State OnTriggerExitState; // None
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If Player enters this region, then insert this region at front of regions list,
+        // and update the region state in Wwise
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("SetMusicState - Player entered " + gameObject.name);
+            RegionStates.Insert(0, OnTriggerEnterState);
+            Debug.Log("SetMusicState - RegionStates list size after entering region is now " + RegionStates.Count);
+            Debug.Log($"SetMusicState - [{string.Join(", ", RegionStates.Select(obj => obj != null ? obj.Name : "NULL"))}]");
+            RegionStates[0].SetValue();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // If Player exits this region...
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("SetMusicState - Player exited " + gameObject.name);
+
+            // ...then remove the innermost matching enclosing region
+            RegionStates.Remove(OnTriggerEnterState);
+            Debug.Log("SetMusicState - RegionStates list size after region removal is now " + RegionStates.Count);
+            Debug.Log($"SetMusicState - [{string.Join(", ", RegionStates.Select(obj => obj != null ? obj.Name : "NULL"))}]");
+            
+            // If there was a region enclosing the removed region...
+            if (RegionStates.Count > 0)
+            {
+                Debug.Log("SetMusicState - Setting new region to the enclosing region");
+                // ...then update region in Wwise to be that enclosing region
+                RegionStates[0].SetValue();
+            }
+            else
+            {
+                Debug.Log("SetMusicState - Setting region to " + OnTriggerExitState.Name);
+                // otherwise there was no enclosing region so update the region
+                // in Wwise to None to signify that Player is now outside of any region
+                OnTriggerExitState.SetValue();
+            }
+        }
+    }
+}
---
+++ Crease_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,82 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMBIENTSWINGS = 2069913319U;
+        static const AkUniqueID PLAY_AMBIENTWATERSPOUT = 827576222U;
+        static const AkUniqueID PLAY_AMBIENTWINDFRUSTRUM1 = 1280143093U;
+        static const AkUniqueID PLAY_AMBIENTWINDFRUSTRUM2 = 1280143094U;
+        static const AkUniqueID PLAY_AMBIENTWINDFRUSTRUM3 = 1280143095U;
+        static const AkUniqueID PLAY_AMBIENTWINDFRUSTRUMGENERAL = 2187218884U;
+        static const AkUniqueID PLAY_COINCOLLECT = 979988297U;
+        static const AkUniqueID PLAY_PLANEBOOST = 3134050369U;
+        static const AkUniqueID PLAY_PLANESPEEDSOUNDS = 2220497489U;
+        static const AkUniqueID PLAY_STICKERCOLLECT = 2321545575U;
+        static const AkUniqueID PLAY_WATERDAMAGE = 94728852U;
+        static const AkUniqueID PLAY_WINDFRUSTRUMLIFT = 1844555015U;
+        static const AkUniqueID START_L1AMBIENCESWITCH = 1987482915U;
+        static const AkUniqueID STOP_L1AMBIENCESWITCH = 3300885361U;
+        static const AkUniqueID TEST_DELETE_ME = 2446149930U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace REGIONAMBIENCESTATES
+        {
+            static const AkUniqueID GROUP = 567225543U;
+
+            namespace STATE
+            {
+                static const AkUniqueID FORESTAREA = 3336434501U;
+                static const AkUniqueID GRASSLAKEAREA = 421983475U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace REGIONAMBIENCESTATES
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PLAYER_FLIGHTSPEED = 4135936604U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID CANYON = 2927127661U;
+        static const AkUniqueID FOLDMENU = 953591617U;
+        static const AkUniqueID FOREST = 491961918U;
+        static const AkUniqueID LAKE = 624189772U;
+        static const AkUniqueID LEVEL1ENDMENU = 879074040U;
+        static const AkUniqueID LEVEL1GENERAL = 1995660790U;
+        static const AkUniqueID MAINMENU = 3604647259U;
+        static const AkUniqueID PAUSEMENU = 3494343696U;
+        static const AkUniqueID SAND_END = 1079185769U;
+        static const AkUniqueID UNDERWATER = 2213237662U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
