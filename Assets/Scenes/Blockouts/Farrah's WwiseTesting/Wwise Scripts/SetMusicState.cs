668d283d43567c560275c01913ff3d6fbefc8ee7
Farrah McQueen <128991498+FMcQueen@users.noreply.github.com>
2026-08-08 13:50:58 -0400

    Updated script to align with coding standards. Re-implemented sfx in latest level1-Combined scene

--- Assets/Scenes/Blockouts/Farrah's WwiseTesting/Wwise Scripts/SetMusicState.cs
+++ Assets/Scenes/Blockouts/Farrah's WwiseTesting/Wwise Scripts/SetMusicState.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class SetMusicState : MonoBehaviour
 {
     // Keeps track of region nesting
-    public static List<AK.Wwise.State> RegionStates = new List<AK.Wwise.State>();
+    private static List<AK.Wwise.State> _regionStates = new List<AK.Wwise.State>();
+    
     public AK.Wwise.State OnTriggerEnterState; // this (gameObject's) region
     public AK.Wwise.State OnTriggerExitState; // None
 
@@ -15,11 +16,14 @@ public class SetMusicState : MonoBehaviour
         // and update the region state in Wwise
         if (other.CompareTag("Player"))
         {
-            Debug.Log("SetMusicState - Player entered " + gameObject.name);
-            RegionStates.Insert(0, OnTriggerEnterState);
-            Debug.Log("SetMusicState - RegionStates list size after entering region is now " + RegionStates.Count);
-            Debug.Log($"SetMusicState - [{string.Join(", ", RegionStates.Select(obj => obj != null ? obj.Name : "NULL"))}]");
-            RegionStates[0].SetValue();
+            // Debug.Log("SetMusicState - Player entered " + gameObject.name);
+            
+            _regionStates.Insert(0, OnTriggerEnterState);
+            
+            // Debug.Log("SetMusicState - RegionStates list size after entering region is now " + _regionStates.Count);
+            // Debug.Log($"SetMusicState - RegionStates: [{string.Join(", ", _regionStates.Select(obj => obj != null ? obj.Name : "NULL"))}]");
+            
+            _regionStates[0].SetValue();
         }
     }
 
@@ -28,24 +32,27 @@ public class SetMusicState : MonoBehaviour
         // If Player exits this region...
         if (other.CompareTag("Player"))
         {
-            Debug.Log("SetMusicState - Player exited " + gameObject.name);
+            // Debug.Log("SetMusicState - Player exited " + gameObject.name);
 
             // ...then remove the innermost matching enclosing region
-            RegionStates.Remove(OnTriggerEnterState);
-            Debug.Log("SetMusicState - RegionStates list size after region removal is now " + RegionStates.Count);
-            Debug.Log($"SetMusicState - [{string.Join(", ", RegionStates.Select(obj => obj != null ? obj.Name : "NULL"))}]");
+            _regionStates.Remove(OnTriggerEnterState);
+            
+            // Debug.Log("SetMusicState - RegionStates list size after region removal is now " + _regionStates.Count);
+            // Debug.Log($"SetMusicState - RegionStates: [{string.Join(", ", _regionStates.Select(obj => obj != null ? obj.Name : "NULL"))}]");
             
             // If there was a region enclosing the removed region...
-            if (RegionStates.Count > 0)
+            if (_regionStates.Count > 0)
             {
-                Debug.Log("SetMusicState - Setting new region to the enclosing region");
+                // Debug.Log("SetMusicState - Setting new region to the enclosing region");
+                
                 // ...then update region in Wwise to be that enclosing region
-                RegionStates[0].SetValue();
+                _regionStates[0].SetValue();
             }
             else
             {
-                Debug.Log("SetMusicState - Setting region to " + OnTriggerExitState.Name);
-                // otherwise there was no enclosing region so update the region
+                // Debug.Log("SetMusicState - Setting region to " + OnTriggerExitState.Name);
+                
+                // ...otherwise there was no enclosing region so update the region
                 // in Wwise to None to signify that Player is now outside of any region
                 OnTriggerExitState.SetValue();
             }
